abd5324feae9971bcfbb73c77000e4df34e0664c
Sadig Muradov <sadig@ymail.com>
2017-09-30 16:04:47 +0400

    Dropbox API changes

--- backuper.sh
+++ backuper.sh
@@ -47,7 +47,7 @@ function setup_dropbox {
 	# Setting up the Dropbox API
 	# - https://github.com/andreafabrizi/Dropbox-Uploader | https://www.andreafabrizi.it/?dropbox_uploader
 	if [ ! -f ./dropbox_uploader.sh ]; then	
-		curl "https://raw.githubusercontent.com/msadig/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
+		curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
 	fi
 	chmod +x dropbox_uploader.sh # make executable
 	./dropbox_uploader.sh # setup Dropbox
